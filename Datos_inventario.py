bfc5089c3e44b2bb0c8f5fdf9f69b55c79eb4e6c
Juan Marcos Cortés Enriquez <124099699+JuanMaCE@users.noreply.github.com>
2024-01-26 18:38:37 -0600

    se subio mi primer avance, donde se muestra como me conecte a la base de datos, y como voy con la parte visual del codigo.

---
+++ Datos_inventario.py
@@ -0,0 +1,67 @@
+import customtkinter
+import sqlite3
+from tkinter import ttk
+
+
+def main():
+    cargar_base_de_datos()
+    # creación de ventana y cosas basicas
+    customtkinter.set_appearance_mode('dark')
+    customtkinter.set_default_color_theme('dark-blue')
+    root = customtkinter.CTk()
+    root.title("Datos de inventario")
+    frame = customtkinter.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill='both', expand=True)
+
+    # cargar frontend
+    front_end(frame)
+
+    root.geometry("1280x720")
+    root.mainloop()
+    return
+
+
+def cargar_base_de_datos():
+    try:
+        conexion = sqlite3.connect("src/database")
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM objetos_de_inventario")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as ex:
+        print(ex)
+
+    return
+
+
+def front_end(frame):
+
+    # front
+    # labels
+    lb_datos_inventario = customtkinter.CTkLabel(master=frame, text='Datos de inventario', font=("Arial", 20))
+    lb_datos_inventario.pack(pady=400, padx=400,)
+    lb_datos_inventario.place(x=10, y=10)
+
+    lb_busqueda = customtkinter.CTkLabel(master=frame, text='Busqueda ', font=("Arial", 20))
+    lb_busqueda.pack(pady=400, padx=400,)
+    lb_busqueda.place(x=10, y=50)
+
+    # text boxs
+    txt_id = customtkinter.CTkEntry(master=frame, placeholder_text='ID de objeto')
+    txt_id.pack(pady=12, padx=10)
+    txt_id.place(x=120, y=50)
+
+    # generar tablas
+
+    table = ttk.Treeview(master=frame, columns=('ID', 'Nombre', 'Caracteristicas'), show='headings')
+    table.heading('ID', text='ID')
+    table.heading('Nombre', text='Nombre')
+    table.heading('Caracteristicas', text='Caracteristicas')
+    table.pack()
+    table.place(x=0, y=150)
+
+    return
+
+
+main()
