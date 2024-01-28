c6de704dc6392351d619f7c433aecb5f7f2560ab
CCVela1017 <124698727+CCVela1017@users.noreply.github.com>
2024-01-27 19:54:53 -0600

    Nuevo diseño de tabla equipos
    Se agrego un nuevo diseño de tabla y un combobox de categoría a equipo

--- datos_equipo.py
+++ datos_equipo.py
@@ -1,31 +1,93 @@
 import customtkinter
 
 from tkinter import ttk
+from tkinter import *
 
 
-def front_end(frame):
-    lb_datos_inventario = customtkinter.CTkLabel(master=frame, text='Equipo', font=("Arial", 20))
+def front_end(frame, frame2):
+
+    lb_datos_inventario = customtkinter.CTkLabel(master=frame, text='Equipo', font=("Times New Roman", 20))
     lb_datos_inventario.pack(pady=400, padx=400, )
     lb_datos_inventario.place(x=10, y=10)
 
-    lb_busqueda = customtkinter.CTkLabel(master=frame, text='Busqueda ', font=("Arial", 20))
+    lb_busqueda = customtkinter.CTkLabel(master=frame, text='Busqueda ', font=("Times New Roman", 20))
     lb_busqueda.pack(pady=400, padx=400, )
     lb_busqueda.place(x=10, y=50)
 
-    txt_id = customtkinter.CTkEntry(master=frame, placeholder_text='ID de objeto')
-    txt_id.pack(pady=12, padx=10)
-    txt_id.place(x=120, y=50)
+    cb_busqueda = customtkinter.CTkComboBox(master=frame, font=("Times New Roman", 20),
+                                            values=['ID', 'Nombre'])
+    cb_busqueda.pack(pady=400, padx=400, )
+    cb_busqueda.place(x=400, y=10)
 
-    table = ttk.Treeview(master=frame, columns=('ID', 'Nombre', 'Caracteristicas', 'Costo sin IVA',
-                                                'Costo con IVA', 'No.Serie'), show='headings')
-    table.heading('ID', text='ID')
-    table.heading('Nombre', text='Nombre')
-    table.heading('Caracteristicas', text='Caracteristicas')
-    table.heading('Costo sin IVA', text='Costo sin IVA')
-    table.heading('Costo con IVA', text='Costo con IVA')
-    table.heading('No.Serie', text='No.Serie')
-    table.pack()
-    table.place(x=0, y=150)
+    lb_combo = customtkinter.CTkLabel(master=frame, text='Buscar por: ', font=("Times New Roman", 20))
+    lb_combo.pack(pady=400, padx=400, )
+    lb_combo.place(x=300, y=10)
+
+    txt_id = customtkinter.CTkEntry(master=frame, placeholder_text='Buscar')
+    txt_id.pack(pady=12, padx=10)
+    txt_id.place(x=100, y=50)
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure('Treeview',
+                    background='#D3D3D3',
+                    foreground='black',
+                    rowheight=25,
+                    fieldbackground='#D3D3D3',
+                    font=('Times New Roman', 12))
+    style.configure('Treeview.Heading', font=('Times New Roman', 12))
+
+    style.map('Treeview',
+              background=[('selected', '#347083')])
+
+    tree_frame = Frame(frame2)
+    tree_frame.pack(pady=10)
+
+    tree_scroll = Scrollbar(tree_frame)
+    tree_scroll.pack(side=RIGHT, fill=Y)
+
+    my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode='extended')
+    my_tree.pack()
+
+    tree_scroll.config(command=my_tree.yview)
+
+    my_tree['columns'] = ('ID', 'Nombre', 'Caracteristicas', 'Costo sin IVA', 'Costo con IVA', 'No.Serie')
+
+    my_tree.column('#0', width=0, stretch=NO)
+    my_tree.column('ID', anchor=W, stretch=NO)
+    my_tree.column('Nombre', anchor=W, stretch=NO)
+    my_tree.column('Caracteristicas', anchor=W, stretch=NO)
+    my_tree.column('Costo sin IVA', anchor=W, stretch=NO)
+    my_tree.column('Costo con IVA', anchor=W, stretch=NO)
+    my_tree.column('No.Serie', anchor=W, stretch=NO)
+
+    my_tree.heading('#0', text='', anchor=W)
+    my_tree.heading('ID', text='ID', anchor=W)
+    my_tree.heading('Nombre', text='Nombre', anchor=W)
+    my_tree.heading('Caracteristicas', text='Caracteristicas', anchor=W)
+    my_tree.heading('Costo sin IVA', text='Costo sin IVA', anchor=W)
+    my_tree.heading('Costo con IVA', text='Costo con IVA', anchor=W)
+    my_tree.heading('No.Serie', text='No.Serie', anchor=W)
+
+    my_tree.tag_configure('oddrow', background='white')
+    my_tree.tag_configure('evenrow', background='lightblue')
+
+    data = [['1', 'Ejemplo1', 'Carac. Ejemplo1', 20, 18, '4D54S5'],
+            ['2', 'Ejemplo2', 'Carac. Ejemplo2', 80, 78, 'd4s5454']]
+
+    global count
+    count = 0
+
+    for record in data:
+        if count % 2 == 0:
+            my_tree.insert(parent='', index='end', iid=count, text='',
+                           values=(record[0], record[1], record[2], record[3],
+                                   record[4], record[5]), tags=('evenrow',))
+        else:
+            my_tree.insert(parent='', index='end', iid=count, text='',
+                           values=(record[0], record[1], record[2], record[3],
+                                   record[4], record[5]), tags=('oddrow',))
+        count += 1
 
     return
 
@@ -38,10 +100,12 @@ def main_window():
     root.title("Datos de Equipo")
     frame = customtkinter.CTkFrame(master=root)
     frame.pack(pady=20, padx=60, fill='both', expand=True)
+    frame2 = customtkinter.CTkFrame(master=frame, height=200, width=20)
+    frame2.pack(pady=100, padx=10, fill='both', expand=True)
 
-    front_end(frame)
+    front_end(frame, frame2)
 
-    root.geometry('1280x720')
+    root.geometry('1280x550')
     root.mainloop()
 
 
