9739d89200a0651bc77b024a2501ace1242f25aa
Juan Marcos Cortés Enriquez <124099699+JuanMaCE@users.noreply.github.com>
2024-01-29 19:22:02 -0600

    se termino al 100 porciento toda la parte visual y el ingreso de datos.

--- compras.py
+++ compras.py
@@ -1,10 +1,174 @@
 import os.path
 import customtkinter
 from PIL import Image
+from tkinter import filedialog
+import shutil
+import os
+
+global texto_imagen
+
+
+def main():
+    ventana = cargar_datos()
+    frame = frame1(ventana)
+    frame2 = frame_2(ventana)
+    color = "#3E4446"
+    labels_parte1(frame, frame2)
+
+    # IB
+    ib_id = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese Codigo')
+    ib_id.pack(pady=12, padx=10)
+    ib_id.place(x=110, y=67)
+
+    ib_name = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese Nombre del producto', width=400,
+                                     height=35)
+    ib_name.pack(pady=100, padx=10)
+    ib_name.place(x=410, y=63)
+    ib_desc = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese una descripción del producto', width=480,
+                                     height=35)
+    ib_desc.pack(pady=100, padx=10)
+    ib_desc.place(x=330, y=106)
+
+    ib_proveedor = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese Nombre del proveedor', width=350,
+                                          height=35)
+    ib_proveedor.pack(pady=100, padx=10)
+    ib_proveedor.place(x=150, y=153)
+
+    ib_precio = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese el precio', width=180, height=35)
+    ib_precio.pack(pady=100, padx=10)
+    ib_precio.place(x=630, y=153)
+
+    ib_cantidad = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese la cantidad', width=180,
+                                         height=35)
+    ib_cantidad.pack(pady=100, padx=10)
+    ib_cantidad.place(x=630, y=200)
+
+    ib_num_factura = customtkinter.CTkEntry(master=frame, placeholder_text='CF/NUM. DE FACTURA',
+                                            width=180, height=35)
+    ib_num_factura.pack(pady=100, padx=10)
+    ib_num_factura.place(x=200, y=245)
+    # buttons
+
+    def seleccionar_imagen():
+        get_image = filedialog.askopenfilenames(title="SELECT IMAGE",
+                                                filetypes=(("png", "*.png"), ("jpg", "*.jpg"), ("Allfile", "*.*")))
+        texto = str(get_image)
+        texto_final = ""
+        for i in range(2, len(texto) - 3):
+            texto_final += texto[i]
+        print(texto_final)
+        ruta_imagen_origen = str(texto_final)
+        ruta_carpeta_destino = 'imagenes'
+        if not os.path.exists(ruta_carpeta_destino):
+            os.makedirs(ruta_carpeta_destino)
+
+        shutil.copy(ruta_imagen_origen, ruta_carpeta_destino)
+        texto_alrevez = ""
+        for i in range(len(texto_final)):
+            texto_alrevez += texto_final[-i-1]
+        texto_final_alrevez = ""
+        for i in range(len(texto_alrevez)):
+            if texto_alrevez[i] == "/":
+                break
+            texto_final_alrevez += texto_alrevez[i]
+        texto_image = ""
+        for i in range(len(texto_final_alrevez)):
+            texto_image += texto_final_alrevez[-i-1]
+        global texto_imagen
+        texto_imagen = texto_image
+
+    # botones
+
+    button_select_img = customtkinter.CTkButton(master=frame, text="seleccionar", fg_color=color,
+                                                command=seleccionar_imagen)
+    button_select_img.pack(pady=100, padx=10)
+    button_select_img.place(x=260, y=207)
+
+    # label
+    lb_mostrar_datos = customtkinter.CTkLabel(master=frame2, text="", font=("Times New Roman", 20))
+
+    lb_mostrar_datos.pack(pady=12, padx=10)
+    lb_mostrar_datos.place(x=10, y=70)
+
+    imagen_carga = 'imagenes/imagen_carga.png'
+
+    # imagen
+    img_path = os.path.join(os.path.dirname(__file__), imagen_carga)
+    image = customtkinter.CTkImage(light_image=Image.open(img_path), size=(150, 150))
+    img_label = customtkinter.CTkLabel(frame2, image=image)
+    img_label.place(x=130, y=150)
+
+    def confirmacion_1():
+        # definir variables
+        id_code = ib_id.get()
+        producto = ib_name.get()
+        descripcion = ib_desc.get()
+        proveedor = ib_proveedor.get()
+        precio = int(ib_precio.get())
+        cantidad = int(ib_cantidad.get())
+        numero_de_factura = ib_num_factura.get()
+        total = precio * cantidad
+        print(texto_imagen + " 1231")
+
+        # texto mostrar
+        txt_mostrar = ("Codigo: " + id_code + " - Producto: " + producto + " - Descripción: " + descripcion +
+                       " - Numero de factura:" + numero_de_factura + '\n' + " - proveedor: " + proveedor)
+        labels_parte2(precio, cantidad, total, frame2)
+        # label mostrar datos
+        lb_mostrar_datos.configure(text=txt_mostrar, fg_color=color)
+        imagen_carga2 = 'imagenes/' + texto_imagen
+        img_path2 = os.path.join(os.path.dirname(__file__), imagen_carga2)
+        image.configure(light_image=Image.open(img_path2), size=(150, 150))
+
+        # desactivar input box
+        ib_id.configure(state="disable")
+        ib_name.configure(state="disable")
+        ib_desc.configure(state="disable")
+        ib_proveedor.configure(state="disable")
+        ib_precio.configure(state="disable")
+        ib_cantidad.configure(state="disable")
+        ib_num_factura.configure(state="disable")
+
+    def confirmacion_2():
+        print("conectar base de datos")
+
+    def editar():
+        # activar input box
+        ib_id.configure(state="normal")
+        ib_name.configure(state="normal")
+        ib_desc.configure(state="normal")
+        ib_proveedor.configure(state="normal")
+        ib_precio.configure(state="normal")
+        ib_cantidad.configure(state="normal")
+        ib_num_factura.configure(state="normal")
+        lb_mostrar_datos.configure(text="", fg_color="transparent")
+
+        imagen_carga2 = 'imagenes/imagen_carga.png'
+        img_path2 = os.path.join(os.path.dirname(__file__), imagen_carga2)
+        image.configure(light_image=Image.open(img_path2), size=(150, 150))
+
+    # confirmación
+    button_confirm_1 = customtkinter.CTkButton(master=frame, text="Confirmar", fg_color=color, width=180, height=45,
+                                               command=confirmacion_1)
+    button_confirm_1.pack(pady=100, padx=10)
+    button_confirm_1.place(x=630, y=240)
+
+    button_confirm_2 = customtkinter.CTkButton(master=frame2, text="Confirmar", fg_color=color, width=180, height=45,
+                                               command=confirmacion_2)
+    button_confirm_2.pack(pady=100, padx=10)
+    button_confirm_2.place(x=630, y=280)
+
+    button_edit = customtkinter.CTkButton(master=frame2, text="Editar", fg_color=color, width=180, height=45,
+                                          command=editar)
+    button_edit.pack(pady=100, padx=10)
+    button_edit.place(x=440, y=280)
+
+    ventana.mainloop()
+
+    return
 
 
 def cargar_datos():
-
     customtkinter.set_appearance_mode('dark')
     customtkinter.set_default_color_theme('dark-blue')
     # root
@@ -84,14 +248,6 @@ def labels_parte1(frame, frame2):
     lb_total_confi.pack(pady=400, padx=400)
     lb_total_confi.place(x=440, y=240)
 
-    imagen_carga = 'imagenes/imagen_carga.png'
-
-    # imagen
-    img_path = os.path.join(os.path.dirname(__file__), imagen_carga)
-    image = customtkinter.CTkImage(light_image=Image.open(img_path), size=(150, 150))
-    img_label = customtkinter.CTkLabel(frame2, image=image)
-    img_label.place(x=130, y=150)
-
 
 def labels_parte2(precio, cantidad, total, frame2):
 
@@ -109,120 +265,6 @@ def labels_parte2(precio, cantidad, total, frame2):
                                             font=("Times New Roman", 30))
     lb_total_confi.pack(pady=400, padx=400)
     lb_total_confi.place(x=440, y=240)
-
-
-def main():
-    ventana = cargar_datos()
-    frame = frame1(ventana)
-    frame2 = frame_2(ventana)
-    color = "#3E4446"
-    labels_parte1(frame, frame2)
-
-    # IB
-    ib_id = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese Codigo')
-    ib_id.pack(pady=12, padx=10)
-    ib_id.place(x=110, y=67)
-
-    ib_name = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese Nombre del producto', width=400,
-                                     height=35)
-    ib_name.pack(pady=100, padx=10)
-    ib_name.place(x=410, y=63)
-    ib_desc = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese una descripción del producto', width=480,
-                                     height=35)
-    ib_desc.pack(pady=100, padx=10)
-    ib_desc.place(x=330, y=106)
-
-    ib_proveedor = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese Nombre del proveedor', width=350,
-                                          height=35)
-    ib_proveedor.pack(pady=100, padx=10)
-    ib_proveedor.place(x=150, y=153)
-
-    ib_precio = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese el precio', width=180, height=35)
-    ib_precio.pack(pady=100, padx=10)
-    ib_precio.place(x=630, y=153)
-
-    ib_cantidad = customtkinter.CTkEntry(master=frame, placeholder_text='Ingrese la cantidad', width=180,
-                                         height=35)
-    ib_cantidad.pack(pady=100, padx=10)
-    ib_cantidad.place(x=630, y=200)
-
-    ib_num_factura = customtkinter.CTkEntry(master=frame, placeholder_text='CF/NUM. DE FACTURA',
-                                            width=180, height=35)
-    ib_num_factura.pack(pady=100, padx=10)
-    ib_num_factura.place(x=200, y=245)
-    # buttons
-
-    button_select_img = customtkinter.CTkButton(master=frame, text="seleccionar", fg_color=color)
-    button_select_img.pack(pady=100, padx=10)
-    button_select_img.place(x=260, y=207)
-
-    # label
-    lb_mostrar_datos = customtkinter.CTkLabel(master=frame2, text="", font=("Times New Roman", 20))
-
-    lb_mostrar_datos.pack(pady=12, padx=10)
-    lb_mostrar_datos.place(x=10, y=70)
-
-    def confirmacion_1():
-        # definir variables
-        id_code = ib_id.get()
-        producto = ib_name.get()
-        descripcion = ib_desc.get()
-        proveedor = ib_proveedor.get()
-        precio = int(ib_precio.get())
-        cantidad = int(ib_cantidad.get())
-        numero_de_factura = ib_num_factura.get()
-        total = precio * cantidad
-
-        # texto mostrar
-        txt_mostrar = ("Codigo: " + id_code + " - Producto: " + producto + " - Descripción: " + descripcion +
-                       " - Numero de factura:" + numero_de_factura + '\n' + " - proveedor: " + proveedor)
-        labels_parte2(precio, cantidad, total, frame2)
-
-        # label mostrar datos
-        lb_mostrar_datos.configure(text=txt_mostrar, fg_color=color)
-
-        # desactivar input box
-        ib_id.configure(state="disable")
-        ib_name.configure(state="disable")
-        ib_desc.configure(state="disable")
-        ib_proveedor.configure(state="disable")
-        ib_precio.configure(state="disable")
-        ib_cantidad.configure(state="disable")
-        ib_num_factura.configure(state="disable")
-
-    def confirmacion_2():
-        print("conectar base de datos")
-
-    def editar():
-        # activar input box
-        ib_id.configure(state="normal")
-        ib_name.configure(state="normal")
-        ib_desc.configure(state="normal")
-        ib_proveedor.configure(state="normal")
-        ib_precio.configure(state="normal")
-        ib_cantidad.configure(state="normal")
-        ib_num_factura.configure(state="normal")
-        lb_mostrar_datos.configure(text="", fg_color="transparent")
-
-    # confirmación
-    button_confirm_1 = customtkinter.CTkButton(master=frame, text="Confirmar", fg_color=color, width=180, height=45,
-                                               command=confirmacion_1)
-    button_confirm_1.pack(pady=100, padx=10)
-    button_confirm_1.place(x=630, y=240)
-
-    button_confirm_2 = customtkinter.CTkButton(master=frame2, text="Confirmar", fg_color=color, width=180, height=45,
-                                               command=confirmacion_2)
-    button_confirm_2.pack(pady=100, padx=10)
-    button_confirm_2.place(x=630, y=280)
-
-    button_edit = customtkinter.CTkButton(master=frame2, text="Editar", fg_color=color, width=180, height=45,
-                                          command=editar)
-    button_edit.pack(pady=100, padx=10)
-    button_edit.place(x=440, y=280)
-
-    ventana.mainloop()
-
-    return
 
 
 main()
