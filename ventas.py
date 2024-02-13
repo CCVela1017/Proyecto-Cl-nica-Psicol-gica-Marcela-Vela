00eb86d0454b4822ccad4a2f87dcb90e3e627507
CCVela1017 <124698727+CCVela1017@users.noreply.github.com>
2024-02-12 22:54:45 -0600

    Cambios en diseño ventas
    Se agregaron funciones al diseño de ventas

--- ventas.py
+++ ventas.py
@@ -52,7 +52,7 @@ def cargar_datos():
     ventana = customtkinter.CTkToplevel()
     ventana.grab_set()
     ventana.title("Ventas")
-    ventana.geometry('950x800+50x50')
+    ventana.geometry('1690x800+50x50')
     ventana.iconbitmap('icon.ico')
     return ventana
 
@@ -67,13 +67,25 @@ def labels_parte1(frame):
     lb_no_fac.pack(pady=400, padx=400, )
     lb_no_fac.place(x=10, y=60)
 
+    entry_no_fac = customtkinter.CTkEntry(master=frame, font=("Times New Roman", 30))
+    entry_no_fac.pack(pady=400, padx=400, )
+    entry_no_fac.place(x=175, y=60)
+
     lb_name = customtkinter.CTkLabel(master=frame, text='Nombre', font=("Times New Roman", 30))
     lb_name.pack(pady=400, padx=400, )
-    lb_name.place(x=10, y=100)
+    lb_name.place(x=10, y=103)
+
+    entry_name = customtkinter.CTkEntry(master=frame, font=("Times New Roman", 30))
+    entry_name.pack(pady=400, padx=400, )
+    entry_name.place(x=175, y=103)
 
     lb_nit = customtkinter.CTkLabel(master=frame, text='Nit', font=("Times New Roman", 30))
     lb_nit.pack(pady=400, padx=400, )
-    lb_nit.place(x=10, y=140)
+    lb_nit.place(x=10, y=145)
+
+    entry_nit = customtkinter.CTkEntry(master=frame, font=("Times New Roman", 30))
+    entry_nit.pack(pady=400, padx=400, )
+    entry_nit.place(x=175, y=145)
 
 
 def labels_parte2(frame):
@@ -90,9 +102,25 @@ def labels_parte2(frame):
     lb_servicio.pack(pady=400, padx=400, )
     lb_servicio.place(x=10, y=10)
 
+    lb_insumos = customtkinter.CTkLabel(master=frame, text='Insumos: ', font=("Times New Roman", 40, "bold"))
+    lb_insumos.pack(pady=400, padx=400, )
+    lb_insumos.place(x=10, y=100)
+
+    lb_insumos2 = customtkinter.CTkLabel(master=frame, text='Insumos utilizados: ', font=("Times New Roman", 40, "bold"))
+    lb_insumos2.pack(pady=400, padx=400, )
+    lb_insumos2.place(x=850, y=10)
+
     combo_box = customtkinter.CTkComboBox(master=frame, values=data_, width=250)
     combo_box.pack(pady=400, padx=400)
     combo_box.place(x=50, y=70)
+
+    boton_add = customtkinter.CTkButton(master=frame,  text='Añadir servicio', font=("Times New Roman", 12, "bold"))
+    boton_add.pack(pady=400, padx=400)
+    boton_add.place(x=310, y=70)
+
+    boton_add2 = customtkinter.CTkButton(master=frame, text='+', font=("Times New Roman", 40, "bold"), width=55)
+    boton_add2.pack(pady=400, padx=400)
+    boton_add2.place(x=750, y=160)
 
     style = ttk.Style()
     style.theme_use('default')
@@ -109,7 +137,7 @@ def labels_parte2(frame):
 
     tree_frame = Frame(frame)
     tree_frame.pack(pady=70)
-    tree_frame.place(x=70, y=120)
+    tree_frame.place(x=70, y=160)
 
     tree_scroll = Scrollbar(tree_frame)
     tree_scroll.pack(side=RIGHT, fill=Y)
@@ -157,6 +185,59 @@ def labels_parte2(frame):
             my_tree.insert(parent='', index='end', iid=count, text='',
                            values=(record[0], record[1], str(record[2]) + ' unidades'
                                    , 'Q' + str(record[3])), tags=('oddrow',))
+        count += 1
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure('Treeview',
+                    background='#D3D3D3',
+                    foreground='black',
+                    rowheight=25,
+                    fieldbackground='#D3D3D3',
+                    font=('Times New Roman', 12))
+    style.configure('Treeview.Heading', font=('Times New Roman', 12))
+
+    style.map('Treeview',
+              background=[('selected', '#347083')])
+
+    tree_frame = Frame(frame)
+    tree_frame.pack(pady=70)
+    tree_frame.place(x=850, y=100)
+
+    tree_scroll = Scrollbar(tree_frame)
+    tree_scroll.pack(side=RIGHT, fill=Y)
+
+    my_tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set, selectmode='extended')
+    my_tree.pack()
+
+    tree_scroll.config(command=my_tree.yview)
+
+    my_tree['columns'] = ('Nombre', 'Cantidad')
+
+    my_tree.column('#0', width=0, stretch=NO)
+    my_tree.column('Nombre', anchor=W, stretch=NO)
+    my_tree.column('Cantidad', anchor=W)
+
+    my_tree.heading('#0', text='', anchor=W)
+    my_tree.heading('Nombre', text='Nombre', anchor=W)
+    my_tree.heading('Cantidad', text='Cantidad', anchor=W)
+
+    my_tree.tag_configure('oddrow', background='white')
+    my_tree.tag_configure('evenrow', background='lightblue')
+
+    # EVENTO CUANDO SE ESCRIBE EN EL TEXT BOX
+
+    data2 = []
+
+    count = 0
+
+    for record in data2:
+        if count % 2 == 0:
+            my_tree.insert(parent='', index='end', iid=count, text='',
+                           values=(record[0], record[1]), tags=('evenrow',))
+        else:
+            my_tree.insert(parent='', index='end', iid=count, text='',
+                           values=(record[0], record[1]), tags=('oddrow',))
         count += 1
 
 
