1249a41d2b54e74e52321feaf8749dd179b16225
Juan Marcos Cortés Enriquez <124099699+JuanMaCE@users.noreply.github.com>
2024-04-13 21:07:52 -0600

    se agrego utildad del mmodulo financiero

--- Utilidad_Bruta.py
+++ Utilidad_Bruta.py
@@ -1,138 +1,22 @@
 import customtkinter
-import tkinter
-from tkinter import *
-from tkinter import ttk
 
 
+def main_utilidad():
+    root = customtkinter.CTk()
 
+    customtkinter.set_appearance_mode('dark')
+    customtkinter.set_default_color_theme('dark-blue')
 
-def rellenartabla():
-    pass
-    for _ in ():
-            my_tree.insert("", "end", values=(_))
-def agregartabla(lugar):
-    """Tabla"""
-    tabla = Frame(lugar)
-    tabla.pack(pady=70)
+    root.title("Utilidad Bruta")
+    root.iconbitmap('icon.ico')
+    frame = customtkinter.CTkFrame(master=root)
+    root.wm_attributes("-topmost", True)
+    frame.pack(pady=10, padx=10, fill='both', expand=True)
+    frame2 = customtkinter.CTkFrame(master=frame, fg_color="#212121", width=50)
+    frame2.pack(pady=10, padx=8, fill='both', expand=True, side='right')
+    frame3 = customtkinter.CTkFrame(master=frame, fg_color="#212121")
+    frame3.pack(pady=20, padx=8, fill='both', expand=True, side='left')
 
-    tabla.place(x=494, y=230)
-
-    tabla_scroll = Scrollbar(tabla)
-    tabla_scroll.pack(side=RIGHT, fill=Y)
-    global my_tree
-    my_tree = ttk.Treeview(tabla, yscrollcommand=tabla_scroll.set, selectmode='extended')
-
-    my_tree.pack()
-
-    tabla_scroll.config(command=my_tree.yview)
-
-    my_tree['columns'] = (
-        'Gasto', 'Costo unitario', 'Cantidad', 'Total' )
-
-    my_tree.column('#0', width=0, stretch=NO)
-    my_tree.column('Gasto', anchor=W, width=60)
-    my_tree.column('Costo unitario', anchor=W, width=100)
-    my_tree.column('Cantidad', anchor=W, width=60)
-    my_tree.column('Total', anchor=W, width=60)
-
-
-    my_tree.heading('#0', text='', anchor=W)
-    my_tree.heading('Gasto', text='Gasto', anchor=W)
-    my_tree.heading('Costo unitario', text='Costo unitario', anchor=W)
-    my_tree.heading('Cantidad', text='Cantidad', anchor=W)
-    my_tree.heading('Total', text='Total', anchor=W)
-
-
-    my_tree.tag_configure('oddrow', background='black')
-    my_tree.tag_configure('evenrow', background='lightblue')
-
-    for idx, fila_id in enumerate(my_tree.get_children()):
-        if idx % 2 == 0:
-            my_tree.item(fila_id, tags=('evenrow',))
-        else:
-            my_tree.item(fila_id, tags=('oddrow',))
-
-
-def agregartexto(lugar):
-
-    """*-*-*-*-*-*-* Etiquetas *-*-*-*-*-*-*"""
-    """Gastos"""
-    etiqueta = customtkinter.CTkLabel(master=lugar, text="Gastos:", font=("times new roman", 28, "bold"))
-    etiqueta.place( x=64, y=65)
-
-
-    """*-*-*-*-*-*-* Cuadros de texto *-*-*-*-*-*-*"""
-    """Gastos"""
-    ingresar_gasto = customtkinter.CTkEntry(master=lugar, placeholder_text="Gastos", width=200,
-                                      height=35, font=("Times New Roman", 20, "bold"))
-    ingresar_gasto.place(x=160, y=65)
-
-
-
-
-
-    """*-*-*-*-*-*-* Etiquetas *-*-*-*-*-*-*"""
-    """Gastos"""
-    etiqueta = customtkinter.CTkLabel(master=lugar, text="Costo Unitario:", font=("times new roman", 28, "bold"))
-    etiqueta.place(x=64, y=130)
-
-    """*-*-*-*-*-*-* Cuadros de texto *-*-*-*-*-*-*"""
-    """Gastos"""
-    ingresar_costo = customtkinter.CTkEntry(master=lugar, placeholder_text="Costo", width=100,
-                                            height=35, font=("Times New Roman", 20, "bold"))
-    ingresar_costo.place(x=260, y=130)
-
-
-
-    """*-*-*-*-*-*-* Etiquetas *-*-*-*-*-*-*"""
-    """Gastos"""
-    etiqueta = customtkinter.CTkLabel(master=lugar, text="Cantidad:", font=("times new roman", 28, "bold"))
-    etiqueta.place(x=394, y=65)
-
-    """*-*-*-*-*-*-* Cuadros de texto *-*-*-*-*-*-*"""
-    """Gastos"""
-    ingresar_cantidad = customtkinter.CTkEntry(master=lugar, placeholder_text="Cantidad", width=100,
-                                            height=35, font=("Times New Roman", 20, "bold"))
-    ingresar_cantidad.place(x=520, y=65)
-
-
-
-
-
-
-
-def agregarbotones(lugar):
-    """*-*-*-*-*-*-* Botones *-*-*-*-*-*-*"""
-    """Aprobar"""
-    actualizar = customtkinter.CTkButton(master=lugar, text="Aceptar", height=100, width=150,
-                                         font=("times new roman", 20, "bold"),
-                                         fg_color="#086EB9")
-    actualizar.place(x=179, y=230)
-
-
-
-def utilidad_bruta():
-    """Apariencia de la ventana"""
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("dark-blue")
-
-    ventana = customtkinter.CTkToplevel()
-    ventana.title("Utilidad Bruta")
-    ventana.wm_attributes("-topmost", False)
-
-    ventana.geometry("732x523")
-
-    """Se crea el marco y todo lo que va a contener"""
-    """Marco"""
-    marco = customtkinter.CTkFrame(master=ventana, width=684, height=475)
-    marco.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-    agregartabla(marco)
-    agregarbotones(marco)
-    agregartexto(marco)
-
-
-    """Se inicia la ventana"""
-    ventana.mainloop()
-
-utilidad_bruta()
+    frame.pack()
+    root.geometry('800x450')
+    root.mainloop()
--- financiero/utilidad.py
+++ financiero/utilidad.py
@@ -14,9 +14,73 @@ def main_utilidad():
     frame.pack(pady=10, padx=10, fill='both', expand=True)
     frame2 = customtkinter.CTkFrame(master=frame, fg_color="#212121", width=50)
     frame2.pack(pady=10, padx=8, fill='both', expand=True, side='right')
-    frame3 = customtkinter.CTkFrame(master=frame, fg_color="#212121")
-    frame3.pack(pady=20, padx=8, fill='both', expand=True, side='left')
+
+    ib_alquiler = customtkinter.CTkEntry(master=frame, placeholder_text='A')
+    ib_alquiler.pack(pady=12, padx=10)
+    ib_alquiler.place(x=150, y=75)
+
+    ib_energeticos = customtkinter.CTkEntry(master=frame, placeholder_text='E')
+    ib_energeticos.pack(pady=12, padx=10)
+    ib_energeticos.place(x=150, y=110)
+
+    ib_transporte = customtkinter.CTkEntry(master=frame, placeholder_text='T')
+    ib_transporte.pack(pady=12, padx=10)
+    ib_transporte.place(x=150, y=145)
+
+    ib_red = customtkinter.CTkEntry(master=frame, placeholder_text='R')
+    ib_red.pack(pady=12, padx=10)
+    ib_red.place(x=150, y=180)
+
+    ib_planillla = customtkinter.CTkEntry(master=frame, placeholder_text='P', state="disabled")
+    ib_planillla.pack(pady=12, padx=10)
+    ib_planillla.place(x=150, y=210)
+
+    def guardar_datos():
+        alquiler = int(ib_alquiler.get())
+        energia = int(ib_energeticos.get())
+        transporte = int(ib_transporte.get())
+        red = int(ib_red.get())
+        root.geometry('370x365')
+
+        total = alquiler + energia + transporte + red
+
+        lb_total = customtkinter.CTkLabel(master=frame, text='Total: ' + str(total), font=(" ", 25))
+        lb_total.pack(pady=400, padx=400)
+        lb_total.place(x=10, y=275)
+
+    button_confirmar = customtkinter.CTkButton(master=frame, text="Confirmar",
+                                               command=guardar_datos)
+    button_confirmar.pack(pady=10, padx=10)
+    button_confirmar.place(x=150, y=250)
+
+    labels(frame)
 
     frame.pack()
-    root.geometry('800x450')
+    root.geometry('370x350')
     root.mainloop()
+
+
+def labels(frame):
+    lb_utilidad_bruta = customtkinter.CTkLabel(master=frame, text='Utilidad Bruta', font=("Times New Roman", 50, "bold"))
+    lb_utilidad_bruta.pack(pady=400, padx=400, )
+    lb_utilidad_bruta.place(x=10, y=0)
+
+    lb_alquiler = customtkinter.CTkLabel(master=frame, text='Alquiler', font=(" ", 25))
+    lb_alquiler.pack(pady=400, padx=400, )
+    lb_alquiler.place(x=10, y=75)
+
+    lb_energeticos = customtkinter.CTkLabel(master=frame, text='Energeticos', font=(" ", 25))
+    lb_energeticos.pack(pady=400, padx=400, )
+    lb_energeticos.place(x=10, y=110)
+
+    lb_transporte = customtkinter.CTkLabel(master=frame, text='Transporte', font=(" ", 25))
+    lb_transporte.pack(pady=400, padx=400)
+    lb_transporte.place(x=10, y=145)
+
+    lb_red = customtkinter.CTkLabel(master=frame, text='Red', font=(" ", 25))
+    lb_red.pack(pady=400, padx=400)
+    lb_red.place(x=10, y=180)
+
+    lb_planilla = customtkinter.CTkLabel(master=frame, text='Planilla', font=(" ", 25))
+    lb_planilla.pack(pady=400, padx=400)
+    lb_planilla.place(x=10, y=210)
